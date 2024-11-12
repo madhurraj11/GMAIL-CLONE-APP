21fdb9e564eb88df0edfd8f741a8a9a4c9faf876
Madhur Chauhan <madhurchauhan1100@gmail.com>
2024-11-12 12:41:10 +0530

    Update isAuthenticated.js

--- backend/middleware/isAuthenticated.js
+++ backend/middleware/isAuthenticated.js
@@ -3,11 +3,10 @@ import jwt from "jsonwebtoken";
 const isAuthenticated = async (req,res,next) => {
     try {
         const token = req.cookies.token;
-       
+       console.log(token);
         if(!token){
             return res.status(401).json({message:"User not authenticated"});
         }
-
         const decode = await jwt.verify(token, process.env.SECRET_KEY);
         if(!decode) {
             return res.status(401).json({message:"Invalid token"});
@@ -15,7 +14,7 @@ const isAuthenticated = async (req,res,next) => {
         req.id = decode.userId;
         next();
     } catch (error) {
-        console.log(error);
+        console.log(error.response ? error.response.data : error.message);
     }
 }
 export default isAuthenticated;
